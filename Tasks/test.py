7669eaf8cd00fe49bbf5e8c6ea03b7ad31e5ce64
Steven Zhang <zhangyuyuyan@gmail.com>
2021-04-15 21:25:11 +0800

    [Task]Delete repeated test procedures of cifar10 & add test procedure for cifar100

--- Tasks/test.py
+++ Tasks/test.py
@@ -20,20 +20,11 @@ def cifar10_classification_test_procedure(save_path):
     model = tf.keras.models.load_model(save_path)
     model.evaluate(x_test, y_test)
 
-def cifar10_classification_test_procedure_with_restNet50(save_path):
-    import Data
 
-    start_time = time.time()
-    x_train, y_train, x_test, y_test = Data.load_data(dataset_name='cifar10', data_dir="./tmp/cifar-10-batches-py/")
-    print("[Test] Load data time: ", time.time() - start_time)
-    model = tf.keras.models.load_model(save_path)
-    model.evaluate(x_test, y_test)
-    
-def cifar10_classification_test_procedure_with_MobileNetV2(save_path):
+def cifar100_classification_test_procedure(save_path):
     import Data
-
     start_time = time.time()
-    x_train, y_train, x_test, y_test = Data.load_data(dataset_name='cifar10', data_dir="./tmp/cifar-10-batches-py/")
+    x_train, y_train, x_test, y_test = Data.load_data(dataset_name='cifar10', data_dir="./tmp/cifar-100-python/")
     print("[Test] Load data time: ", time.time() - start_time)
     model = tf.keras.models.load_model(save_path)
     model.evaluate(x_test, y_test)
