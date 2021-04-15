93c3532fa631887c8638456b4ae8303cfe359e86
Steven Zhang <zhangyuyuyan@gmail.com>
2021-04-16 03:44:00 +0800

    [Data] fix load data problem in cifa100

--- Data/cifar_100_data.py
+++ Data/cifar_100_data.py
@@ -18,10 +18,10 @@ def load_data(dir_path):
 
     for each_label, each_data in zip(train_d[b'fine_labels'], train_d[b'data']):
         y_train.append(each_label)
-        x_train.append(np.transpose(each_data.reshape((3, 32, 32)), (2, 1, 0)))
+        x_train.append(np.transpose(each_data.reshape((3, 32, 32)), (1, 2, 0)))
     for each_label, each_data in zip(test_d[b'fine_labels'], test_d[b'data']):
         y_test.append(each_label)
-        x_test.append(np.transpose(each_data.reshape((3, 32, 32)), (2, 1, 0)))
+        x_test.append(np.transpose(each_data.reshape((3, 32, 32)), (1, 2, 0)))
     
     x_train, x_test = np.array(x_train), np.array(x_test)
     y_train, y_test = np.array(y_train), np.array(y_test)
