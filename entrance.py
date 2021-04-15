9a6884ebc70f60b883b06f4498f9bba22e578530
Steven Zhang <zhangyuyuyan@gmail.com>
2021-04-15 21:40:10 +0800

    [Entrance] Add All entrance case for cifar10 & cifar100

--- entrance.py
+++ entrance.py
@@ -5,15 +5,90 @@ import Tasks.train as train_porcedure
 import Tasks.test as test_porcedure
 
 if __name__ == '__main__':
-    # save_path = "./tmp/mnist/"
+    
+    #*************************************************************************************
+    #                  MNIST Train & Test
+    #
+    #*************************************************************************************
+    
+    # save_path = "./tmp/model/mnist/"
+    # print("Train MNIST with mnist_classfication_train_procedure")
     # train_porcedure.mnist_classfication_train_procedure(save_path=save_path, epoch=5)
     # test_porcedure.mnist_classification_test_procedure(save_path=save_path)
-    # save_path = "./tmp/cifar_model/"
+    
+    
+    #*************************************************************************************
+    #                  CIFAR10 Train & Test
+    #
+    #*************************************************************************************
+    
+    # save_path = "./tmp/model/cifar_model/"
+    # print("*"*80)
+    # print("Train CIFAR10 with cifar10_classfication_train_procedure")
     # train_porcedure.cifar10_classfication_train_procedure(save_path=save_path, epoch=5)
     # test_porcedure.cifar10_classification_test_procedure(save_path=save_path)
-    # save_path = "./tmp/cifar_restNet50_model/"
+    
+    # save_path = "./tmp/model/cifar_restNet50_model/"
+    # print("*"*80)
+    # print("Train CIFAR10 with cifar10_classfication_train_procedure_with_resNet50")
     # train_porcedure.cifar10_classfication_train_procedure_with_resNet50(save_path=save_path, epoch=130)
-    # test_porcedure.cifar10_classification_test_procedure_with_restNet50(save_path=save_path)
-    save_path = "./tmp/cifar__MobileNetV2_model/"
-    train_porcedure.cifar10_classfication_train_procedure_with_MobileNetV2(save_path=save_path, epoch=130)
-    test_porcedure.cifar10_classification_test_procedure_with_MobileNetV2(save_path=save_path)
+    # test_porcedure.cifar10_classification_test_procedure(save_path=save_path)
+    
+    # save_path = "./tmp/model/cifar_restNet50V2_model/"
+    # print("*"*80)
+    # print("Train CIFAR10 with cifar10_classfication_train_procedure_with_resNet50V2")
+    # train_porcedure.cifar10_classfication_train_procedure_with_resNet50V2(save_path=save_path, epoch=130)
+    # test_porcedure.cifar10_classification_test_procedure(save_path=save_path)
+     
+    # save_path = "./tmp/model/cifar__MobileNet_model/"
+    # print("*"*80)
+    # print("Train CIFAR10 with cifar10_classfication_train_procedure_with_MobileNet")
+    # train_porcedure.cifar10_classfication_train_procedure_with_MobileNet(save_path=save_path, epoch=130)
+    # test_porcedure.cifar10_classification_test_procedure(save_path=save_path)
+    
+    # save_path = "./tmp/model/cifar__MobileNetV2_model/"
+    # print("*"*80)
+    # print("Train CIFAR10 with cifar10_classfication_train_procedure_with_MobileNetV2")
+    # train_porcedure.cifar10_classfication_train_procedure_with_MobileNetV2(save_path=save_path, epoch=130)
+    # test_porcedure.cifar10_classification_test_procedure(save_path=save_path)
+    
+    # save_path = "./tmp/model/cifar__DenseNet121_model/"
+    # print("*"*80)
+    # print("Train CIFAR10 with cifar10_classfication_train_procedure_with_DenseNet121")
+    # train_porcedure.cifar10_classfication_train_procedure_with_DenseNet121(save_path=save_path, epoch=130)
+    # test_porcedure.cifar10_classification_test_procedure(save_path=save_path)
+    
+    #*************************************************************************************
+    #                  CIFAR100 Train & Test
+    #
+    #*************************************************************************************
+    
+    # save_path = "./tmp/model/cifar100_restNet50_model/"
+    # print("*"*80)
+    # print("Train CIFAR100 with cifar100_classfication_train_procedure_with_resNet50")
+    # train_porcedure.cifar100_classfication_train_procedure_with_resNet50(save_path=save_path, epoch=130)
+    # test_porcedure.cifar100_classification_test_procedure(save_path=save_path)
+    
+    # save_path = "./tmp/model/cifar100_restNet50V2_model/"
+    # print("*"*80)
+    # print("Train CIFAR100 with cifar100_classfication_train_procedure_with_resNet50V2")
+    # train_porcedure.cifar100_classfication_train_procedure_with_resNet50V2(save_path=save_path, epoch=130)
+    # test_porcedure.cifar100_classification_test_procedure(save_path=save_path)
+     
+    # save_path = "./tmp/model/cifar100__MobileNet_model/"
+    # print("*"*80)
+    # print("Train CIFAR100 with cifar100_classfication_train_procedure_with_MobileNet")
+    # train_porcedure.cifar100_classfication_train_procedure_with_MobileNet(save_path=save_path, epoch=130)
+    # test_porcedure.cifar100_classification_test_procedure(save_path=save_path)
+    
+    # save_path = "./tmp/model/cifar100__MobileNetV2_model/"
+    # print("*"*80)
+    # print("Train CIFAR100 with cifar100_classfication_train_procedure_with_MobileNetV2")
+    # train_porcedure.cifar100_classfication_train_procedure_with_MobileNetV2(save_path=save_path, epoch=130)
+    # test_porcedure.cifar100_classification_test_procedure(save_path=save_path)
+    
+    # save_path = "./tmp/model/cifar100__DenseNet121_model/"
+    # print("*"*80)
+    # print("Train CIFAR100 with cifar100_classfication_train_procedure_with_DenseNet121")
+    # train_porcedure.cifar100_classfication_train_procedure_with_DenseNet121(save_path=save_path, epoch=130)
+    # test_porcedure.cifar100_classification_test_procedure(save_path=save_path)
